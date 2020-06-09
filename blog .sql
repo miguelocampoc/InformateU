40ec0ffa365b6105709e6859e04a0e487f32e5a8
miguel ocampo <miguelocampoc@gmail.com>
2020-06-09 18:36:47 -0500

    323232322

--- blog .sql
+++ blog .sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-06-2020 a las 00:03:29
+-- Tiempo de generación: 10-06-2020 a las 01:35:41
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.2.26
 
@@ -56,18 +56,23 @@ CREATE TABLE `comentarios` (
   `idrespuesta` int(10) NOT NULL,
   `descripcion` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
   `idpublicacion` int(10) NOT NULL,
-  `iduser` int(10) NOT NULL
+  `iduser` int(10) NOT NULL,
+  `file` varchar(50) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `comentarios`
 --
 
-INSERT INTO `comentarios` (`idrespuesta`, `descripcion`, `idpublicacion`, `iduser`) VALUES
-(86, 'Prueba', 389, 196),
-(90, 'prueba\r\n', 389, 196),
-(91, 'prueba', 391, 196),
-(92, 'prueba', 391, 196);
+INSERT INTO `comentarios` (`idrespuesta`, `descripcion`, `idpublicacion`, `iduser`, `file`) VALUES
+(1, 'Hola. mucho gusto. Espero poder  sacarle un gran provecho a esta red social ', 393, 224, 'NULL'),
+(2, 'Es una buena iniciativa para compartir conocimiento de interés que permita mejorar nuestros conocimientos de una forma divertida y sencilla ', 393, 228, 'NULL'),
+(3, 'Gracias a ti por pertenecer a esta valiosa comunidad', 395, 227, 'NULL'),
+(4, 'Gracias compañero, disfruta del conocimiento', 395, 224, 'NULL'),
+(5, 'Gracias esa informacion tan  valiosa para la comunidad', 402, 227, 'NULL'),
+(6, 'Gracias compañero.', 402, 224, 'NULL'),
+(7, 'Gracias por el aporte, Saludos', 403, 227, 'NULL'),
+(8, 'Gracias  compañero', 403, 224, 'NULL');
 
 -- --------------------------------------------------------
 
@@ -86,14 +91,8 @@ CREATE TABLE `likes` (
 --
 
 INSERT INTO `likes` (`idlike`, `idpublicacion`, `iduser`) VALUES
-(214, 384, 197),
-(216, 383, 197),
-(217, 382, 197),
-(231, 382, 196),
-(234, 384, 196),
-(236, 383, 196),
-(237, 391, 196),
-(239, 389, 196);
+(240, 393, 224),
+(241, 395, 228);
 
 -- --------------------------------------------------------
 
@@ -105,7 +104,7 @@ CREATE TABLE `publicaciones` (
   `idpublicacion` int(10) NOT NULL,
   `descripcion` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
   `iduser` int(10) NOT NULL,
-  `archivo` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `archivo` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
   `likes` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
@@ -114,11 +113,10 @@ CREATE TABLE `publicaciones` (
 --
 
 INSERT INTO `publicaciones` (`idpublicacion`, `descripcion`, `iduser`, `archivo`, `likes`) VALUES
-(382, 'Prueba documento', 196, 'Taller1_MiguelOcampo_07052020.pdf', 2),
-(383, ' Prueba documento ', 196, 'NAT-ESTATICA-Y-DINAMICA-TRABAJOI.pdf', 2),
-(384, 'Prueba imagen', 196, 'carro.jpg', 2),
-(389, ' Prueba', 196, 'carro.jpg', 1),
-(391, '   Prueba', 196, 'Modelo-de-Transbordo.xlsx', 1);
+(393, '  Bienvenidos a migsed, una red social que permite compartir nuestros conocimientos para lograr un mejor desempeño a nivel academico y profesional  ', 227, 'NULL', 1),
+(395, ' Hola gracias por crear este gran espacio para el conocimiento', 228, 'NULL', 1),
+(402, ' Hola comunidad, encontré el siguiente pdf en donde enseñan los conceptos básicos de la seguridad informática, el libro esta en ingles pero es muy sencillo y practico', 229, 'Pentesting.pdf', 0),
+(403, 'Buenas noches, les comparto la siguiente información sobre una de las metodologias agiles para el desarrollo de software llamado scrum.', 230, 'scrumprocess.jpg', 0);
 
 -- --------------------------------------------------------
 
@@ -151,8 +149,11 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`iduser`, `nombre`, `usuario`, `email`, `clave`, `token`, `foto`, `DateTimeRecover`, `data_register`, `apellidos`, `TokenActivate`, `tipo`, `genero`, `cumpleaños`, `biografia`, `idcarrera`, `tokenUpdateEmail`) VALUES
-(196, 'miguel', 'miguel09', 'miguelocampoc@gmail.com', '$argon2i$v=19$m=65536,t=4,p=1$OGhLMksubGRSbHBsaS5TeQ$F+fsi5hvrF8g3rnusxEa18bXhqDWoeOhEWNKTKvqcVU', 'NULL', 'fotouser196.jpg', '2020-05-21 13:21:00', '2020-05-21 13:21:00', 'Ocampo', '$argon2i$v=19$m=65536,t=4,p=1$VDNxZWdjdUZuOTBSaU5PRA$N8sO1dVfIvB5AepYFZr2lDnSyqR+OlIag+3TFJFG9sM', 'administrador', '', '0000-00-00', '', 10000, 'NULL'),
-(223, 'Julio', 'julio09', 'magelosac@gmail.com', '$argon2i$v=19$m=65536,t=4,p=1$aFdnRU1mVHJpci5IT2EuVQ$ylx4KovQgNwazIqXBpZDJxjANiMY8de6Y6JhPwm4xNU', 'NULL', 'NULL', '2020-06-03 21:30:00', '2020-06-03 21:30:00', 'diaz', 'NULL', 'administrador', '', '0000-00-00', '', 10000, '');
+(224, 'ricardo', 'ricardo09', 'magelosac@gmail.com', '$argon2i$v=19$m=65536,t=4,p=1$WFZJa1ZyRkI3THNQRjJqQg$APCQhpJyzDDwVexRFMzPurS8V4whDDwyC0co/1lmZ94', 'NULL', 'fotouser224.jpg', '2020-06-09 20:01:00', '2020-06-09 20:01:00', 'andres', 'NULL', 'Normal', '', '0000-00-00', '', 10000, ''),
+(227, 'miguel', 'ocampo09', 'miguelocampoc@gmail.com', '$argon2i$v=19$m=65536,t=4,p=1$em90bi9TbVRZQXpsaDhLWQ$LHYCxpnRV56yMtmidyCcgXb7watURSr49G3jLQoLNGc', 'NULL', 'fotouser227.jpg', '2020-06-09 20:12:00', '2020-06-09 20:12:00', 'ocampo', 'NULL', 'administrador', '', '0000-00-00', '', 10000, ''),
+(228, 'andres', 'hernan09', 'woldengold@gmail.com', '$argon2i$v=19$m=65536,t=4,p=1$MkNjN3JVTHp2WHhYZmovNQ$unJI4JvFHcqm7fa/zHw0yG+G5hGK6NJkBv0qD1G4li0', 'NULL', 'fotouser228.jpg', '2020-06-09 20:49:00', '2020-06-09 20:49:00', 'hernandez', 'NULL', 'Normal', '', '0000-00-00', '', 10000, ''),
+(229, 'julio', 'gonzales09', 'infomigsed@gmail.com', '$argon2i$v=19$m=65536,t=4,p=1$OWFjLjBxYkNmYlBHTTBYdw$k9jlLUwo9LhuvDPC77hAT5GDyS/SCvY1nbV6860PnMY', 'NULL', 'fotouser229.png', '2020-06-09 21:01:00', '2020-06-09 21:01:00', 'gonzales', 'NULL', 'administrador', '', '0000-00-00', '', 10000, ''),
+(230, 'bernal', 'bernal09', 'miguelocampoch@gmail.com', '$argon2i$v=19$m=65536,t=4,p=1$OFdmN0xPa1VVNWQ3SnViVw$fLWwxZLll/QhYCi8Gs3kTNlUSaHub3bmTdW6ew3GL0E', 'NULL', 'NULL', '2020-06-09 21:08:00', '2020-06-09 21:08:00', 'andres', 'NULL', 'Normal', '', '0000-00-00', '', 10000, '');
 
 --
 -- Índices para tablas volcadas
@@ -208,25 +209,25 @@ ALTER TABLE `carreras`
 -- AUTO_INCREMENT de la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
-  MODIFY `idrespuesta` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
+  MODIFY `idrespuesta` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=128;
 
 --
 -- AUTO_INCREMENT de la tabla `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `idlike` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=240;
+  MODIFY `idlike` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=242;
 
 --
 -- AUTO_INCREMENT de la tabla `publicaciones`
 --
 ALTER TABLE `publicaciones`
-  MODIFY `idpublicacion` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=392;
+  MODIFY `idpublicacion` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=404;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `iduser` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=224;
+  MODIFY `iduser` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=231;
 
 --
 -- Restricciones para tablas volcadas
